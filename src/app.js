4e6256f0281b1922056604fcc260eee915b01818
Martin Kim <martinkim7677@gmail.com>
2018-07-13 20:59:44 -0400

    Basic Webpack setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import validator from 'validator';
+
+console.log(validator.isEmail('test@gmail.com'));
+// import './utils.js';
+// import substract, { square, add } from './utils.js'; 
+// import isSenior, { isAdult, canDrink } from './person.js';
+
+// // console.log(square(4));
+// // console.log(add(100, 23));
+// // console.log(substract(100, 81));
+
+// console.log('Is 18 years old adult? ', isAdult(18));
+// console.log('Is 18 years old can drink? ', canDrink(18));
+// console.log('Is senior?', isSenior(65));
